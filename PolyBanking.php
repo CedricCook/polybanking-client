3e9cc9e8ad8729164564ab65da02e913bcecbafb
Fabian Bircher <f.bircher@gmail.com>
2015-03-10 10:43:16 +0100

    tidy private method

--- PolyBanking.php
+++ PolyBanking.php
@@ -3,13 +3,13 @@
 /**
  * Class PolyBanking
  */
-class PolyBanking {    
+class PolyBanking {
     protected $server = null;
     protected $configID = null;
     protected $keyRequest = null;
     protected $keyAPI = null;
     protected $keyIPN = null;
-    
+
     /**
      * Standard constructor
      */
@@ -32,25 +32,20 @@ class PolyBanking {
      * @return: A hash of the $secret and $data following the official PolyBanking API spec.
      */
     function compute_sig($secret, $data){
-        
-        function escape_for_signature($str){
-            return str_replace(array('=', ';'), array('!!', '??'), $str);
-        }
-        
         $sig = "";
-        
+
         foreach($data as $key => $value){
-            $sig .= escape_for_signature($key);
+            $sig .= $this->escape_for_signature($key);
             $sig .= '=';
-            $sig .= escape_for_signature($value);
+            $sig .= $this->escape_for_signature($value);
             $sig .= ';';
             $sig .= $secret;
             $sig .= ';';
         }
-        
-        return openssl_digest($sig, 'sha512');    
+
+        return openssl_digest($sig, 'sha512');
     }
-    
+
     /**
      *  POST an array to a certain url
      *  @param: $url, the url to post to; $data, an associative array to be posted
@@ -58,7 +53,7 @@ class PolyBanking {
      */
     function post_curl($url, $data){
         $ch = curl_init();
-        
+
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
@@ -66,13 +61,13 @@ class PolyBanking {
 
         //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
         //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        
+
         $result = curl_exec($ch);
 
         curl_close($ch);
         return json_decode($result);
     }
-    
+
     /**
      * Send off a new transaction to the PolyBanking server,
      * then send off the user to their payment site when PolyBanking gives the OK.
@@ -80,24 +75,24 @@ class PolyBanking {
      * @return: an array of the transaction status and the url to send the user to.
      *
      */
-    function new_transaction($amount, $reference, $extra_data = ''){       
+    function new_transaction($amount, $reference, $extra_data = ''){
         $data['amount'] = $amount;
         $data['reference'] = $reference;
         $data['extra_data'] = $extra_data;
         $data['config_id'] = $this->configID;
-        
+
         $data['sign'] = $this->compute_sig($this->keyRequest, $data);
-        
+
         $url = $this->server . "paiements/start/";
-        
+
         $result = $this->post_curl($url, $data);
-        
+
         return $result;
     }
-    
+
     /**
      * Get the details of a specific transaction via its reference
-     * 
+     *
      * @param: $reference, the reference of the transaction you want details for.
      * @return: (reference, extra_data, amount, postfinance_id, postfinance_status, internal_status, ipn_needed,
      *   creation_date, last_userforwarded_date, last_user_back_from_postfinance_date, last_postfinance_ipn_date,
@@ -107,17 +102,17 @@ class PolyBanking {
         if($reference = null){
           throw new InvalidArgumentException('Reference can not be null');
         }
-        
+
         $url = $this->server . "api/transactions/". $reference . "/";
-        
+
         $data['config_id'] = $this->configID;
         $data['secret'] = $this->keyAPI;
-        
+
         //$result now contains all details of the transaction.
         return $this->post_curl($url, $data);
     }
-    
-    
+
+
     /**
      * Get a list references of transactions
      *
@@ -127,20 +122,20 @@ class PolyBanking {
      */
     function get_transactions($max_transaction = 100){
         $url = $this->server . "api/transactions/";
-        
+
         $data['config_id'] = $this->configID;
         $data['secret'] = $this->keyAPI;
         $data['max_transaction'] = $max_transaction;
-        
+
         $result = $this->post_curl($url, $data);
-        
+
         if($result->result != "ok"){
           throw new UnexpectedValueException('Could not get a list of transactions for an unknown reason.');
         }
-        
+
         return $result->data;
     }
-    
+
     /**
      * Show the logs for one transaction
      * @param: $reference, the reference of the transaction you want the logs of.
@@ -150,21 +145,21 @@ class PolyBanking {
         if($reference = null){
           throw new InvalidArgumentException('Reference can not be null');
         }
-        
+
         $url = $this->server . "api/transactions/" . $reference . "/logs/";
-        
+
         $data['config_id'] = $this->configID;
         $data['secret'] = $this->keyAPI;
-        
+
         $result = $this->post_curl($url, $data);
-        
+
         if($result->result != "ok"){
           throw new UnexpectedValueException('Could not get a list of transactions for an unknown reason.');
         }
-        
+
         return $result->data;
     }
-    
+
     /**
      * The PolyBanking server calls our IPN URL to send an IPN notification.
      * This function checks whether the IPN is correct, to see if the transaction was ok (TODO: is this description correct?)
@@ -177,18 +172,26 @@ class PolyBanking {
                 $data[$key] = $value;
             }
         }
-        
+
         if($_POST['sign'] != $this->compute_sig($this->keyIPN, $data)){
             return array(false, 'SIGN', null, null, null, null);
         }
         if($data['config_id'] != $this->configID){
             return array(false, 'CONFIG', null, null, null, null);
         }
-        
-        //TODO: format last_update    
+
+        //TODO: format last_update
         return array(true, '', $data['reference'], $data['postfinance_status'], $data['postfinance_status_good'] == 'True', $data['last_update']);
     }
 
+    /**
+     * Escape a string according to the PolyBanking specs.
+     * @param $str
+     * @return mixed
+     */
+    private function escape_for_signature($str){
+      return str_replace(array('=', ';'), array('!!', '??'), $str);
+    }
 }
 
 ?>
